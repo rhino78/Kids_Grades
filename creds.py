f243d28e0ce71027bbc5c5963d908cbebce67263
Ryan Shave <rshave@gmail.com>
2016-03-24 16:58:41 +0000

    adding the ignore file

---
+++ creds.py
@@ -0,0 +1,6 @@
+USERNAME = 'rshave@gmail.com'
+PASSWORD = 'Thomas08!'
+FROMADDR = 'rshave@gmail.com'
+TOADDR = ['5126899956@messaging.sprintpcs.com', '5127844398@messaging.sprintpcs.com']
+LOGIN = 'carolbraus'
+LOGINPW = 'maxine77'
